0171342754a802931741720301024bf8547455fe
Mariana Moreira Silveira <mariana_furtuoso@hotmail.com>
2021-03-05 08:01:37 -0300

    Add Composição de Objeto

--- Acessando atribuitos de objetos/src/Carro.java
+++ Acessando atribuitos de objetos/src/Carro.java
@@ -5,4 +5,6 @@ public class Carro {
 	String modelo;
 	String cor;
 	int anoDeFabricação;
+	
+	Proprietario dono;
 }
---
+++ Acessando atribuitos de objetos/src/Principal1.java
@@ -0,0 +1,17 @@
+
+public class Principal1 {
+	
+		public static void main(String[] args) {
+			Carro meuCarro = new Carro();
+			meuCarro.anoDeFabricação = 2011;
+			meuCarro.cor = "Prata";
+			meuCarro.fabricante = "Fiat";
+			meuCarro.modelo = "Palio";
+			
+			meuCarro.dono = new Proprietario();
+			meuCarro.dono.nome = "Joao da Silva";
+			meuCarro.dono.bairro = "Centro";
+			
+		}
+
+}
