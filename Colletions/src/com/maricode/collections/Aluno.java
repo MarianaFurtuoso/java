8eb7f12853bfa365112f53c9b2974af2263cbb9f
Mariana <mariana_furtuoso@hotmail.com>
2021-08-25 08:33:33 -0300

    Estudo sobre colletions e orderação

---
+++ Colletions/src/com/maricode/collections/Aluno.java
@@ -0,0 +1,30 @@
+package com.maricode.collections;
+
+public class Aluno {
+	
+		private String nome;
+		private int idade;
+		
+		public Aluno(String nome) {
+				this.setNome(nome);
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public int getIdade() {
+			return idade;
+		}
+
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
+		
+		
+
+}
